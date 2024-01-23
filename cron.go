2f95ef52e400623ae19c75d40cfa3378435a78c5
Sunny Phung <sp@arconomy.digital>
2024-01-24 08:52:19 +1000

    Fix UpdateNextSchedule to prevent gapping

--- cron.go
+++ cron.go
@@ -98,17 +98,17 @@ func (s byTime) Less(i, j int) bool {
 //
 // Available Settings
 //
-//   Time Zone
-//     Description: The time zone in which schedules are interpreted
-//     Default:     time.Local
+//	Time Zone
+//	  Description: The time zone in which schedules are interpreted
+//	  Default:     time.Local
 //
-//   Parser
-//     Description: Parser converts cron spec strings into cron.Schedules.
-//     Default:     Accepts this spec: https://en.wikipedia.org/wiki/Cron
+//	Parser
+//	  Description: Parser converts cron spec strings into cron.Schedules.
+//	  Default:     Accepts this spec: https://en.wikipedia.org/wiki/Cron
 //
-//   Chain
-//     Description: Wrap submitted jobs to customize behavior.
-//     Default:     A chain that recovers panics and logs them to stderr.
+//	Chain
+//	  Description: Wrap submitted jobs to customize behavior.
+//	  Default:     A chain that recovers panics and logs them to stderr.
 //
 // See "cron.With*" to modify the default behavior.
 func New(opts ...Option) *Cron {
@@ -378,7 +378,7 @@ func (c *Cron) UpdateNextSchedule(entry *Entry) {
 	c.runningMu.Lock()
 	defer c.runningMu.Unlock()
 	entry.Prev = entry.Next
-	entry.Next = entry.Schedule.Next(c.now())
+	entry.Next = entry.Schedule.Next(entry.Prev)
 }
 
 func (c *Cron) GetCustomTime() *time.Time {
